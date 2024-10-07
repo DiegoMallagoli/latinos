52573637778c05acaa349eb098ab2ab73559c784
DiegoMallagoli <reisdiego121@gmail.com>
2024-10-07 10:55:53 -0300

    teste final

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function exibirMensagem() {
+        alert("Estamos te encarregando para a outra página!! Confirme em 'OK' para prosseguir");
+    }
+    document.getElementById('Menu').onclick = exibirMensagem;
+    });
+    function exibirMensagem() {
+        alert("Estamos te encarregando para a outra página!! Confirme em 'OK' para prosseguir");
+    }
